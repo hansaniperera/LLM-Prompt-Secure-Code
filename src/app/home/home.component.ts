e2f28fe46c9370f24b43ccbb23f50be34bdfac27
hansaniperera <perera.hansani95@gmail.com>
2025-06-14 19:28:41 +1200

    Login Scenario - gpt generated code

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,8 @@
+// home.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `<h1>Welcome to the Asset Management System</h1>`,
+})
+export class HomeComponent {}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+// login.component.ts
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  loginError = '';
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: () => this.router.navigate(['/home']),
+        error: err => this.loginError = 'Login failed. Please try again.'
+      });
+    }
+  }
+}
